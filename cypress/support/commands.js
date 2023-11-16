0a58b35e8b5833956c90e32ca28b895bc420b626
Alias-Puravathu <alias.puaravthu@ibm.com>
2023-11-16 11:39:08 +0530

    enter username

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,14 @@
 
   export function sendTextToInputElement(textToEnter,inputElement) {
     try {
+        cy.log('Going to type text '+textToEnter+' in element '+inputElement );
         cy.get(inputElement) // No need to specify the timeout, it will use the global default
           .should('be.visible')
           .should('be.enabled')
           .click()
           .clear()
           .type(textToEnter);
+        cy.log('Text '+textToEnter+' typed in element '+inputElement );
       } catch (error) {
         // Handle the error, for example, log it
         cy.log(`Error: ${error.message}`);
