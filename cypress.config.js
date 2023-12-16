d42502e8fcb9c46794779426766a7790ba045abc
Alias-Puravathu <alias.puaravthu@ibm.com>
2023-12-16 23:30:42 +0530

    corrected yml

--- cypress.config.js
+++ cypress.config.js
@@ -17,5 +17,6 @@ module.exports = defineConfig({
     },
     specPattern: "cypress/e2e/*.feature",
     baseUrl: 'https://demo.guru99.com/test/newtours/', // this is your app
+    video: false,
   },
 });
